dee6c57ce4be1aebb640e63d5af0bc2a0e925e1e
leonardo fierro <planeaweb@gmail.com>
2025-02-19 05:43:34 +0000

    cambios de nuevo

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,11 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-  base: "/poliservicios/", // Asegúrate que coincide con la URL
+  plugins: [react()],
+  base: "/poliservicios/",  // Asegura que todos los archivos usen este prefijo en las rutas
   build: {
-    outDir: "dist",
+    outDir: "dist",  // Carpeta donde se guardan los archivos finales
   },
 });
-
-
-
-
-/*
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: "/poliservicios/",
-  plugins: [react()],
-})
-*/
 
