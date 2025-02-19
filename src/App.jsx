eebb5b0e3a45ee139691739016c385692366c022
leonardo fierro <planeaweb@gmail.com>
2025-02-19 22:57:23 +0000

    Cre modules para css hasta el momento header

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,6 @@ import Header from './components/Header';
 import Navbar from './components/Navbar'; // Asegúrate de que la ruta sea correcta
 import About from './components/About'; // O AboutSection
 import Services from './components/Services'; // O ServicesSection
-import Features from './components/Features'; // Importa el nuevo componente
-import Testimonials from './components/Testimonials'; // O TestimonialsSection
-import VideoSection from './components/VideoSection'; // Si es relevante
-import Grids3 from './components/Grids3'; // Si es relevante
 import Footer from './components/Footer';
 import Contact from './components/Contact';
 import Home from './components/Home'; // Asegúrate de importar el nuevo componente Home
@@ -15,14 +11,13 @@ import './App.css'; // Asegúrate de importar tus estilos
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 
+
 const App = () => {
     return (
         <Router basename="/poliservicios"> 
             <div>
-                
                 <Header />
-               
-                <Routes>
+                 <Routes>
                     <Route path="/" element={<Home />} /> {/* Componente Home */}
                     <Route path="/about" element={<About />} />
                     <Route path="/services" element={<Services />} />
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,6 @@
 // src/components/Footer.jsx
 import React from 'react';
+import styles from './Footer.module.css';
 
 const Footer = () => {
     return (
