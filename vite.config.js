7e74ef1768bab2068e8fed59d5e47db28954d9f6
leonardo fierro <planeaweb@gmail.com>
2025-02-19 13:20:03 +0000

    Corrigiendo importación de CSS

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+/*import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
@@ -8,4 +8,14 @@ export default defineConfig({
     outDir: "dist",  // Carpeta donde se guardan los archivos finales
   },
 });
+*/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+export default defineConfig({
+  plugins: [react()],
+  base: "./",  // Usa "./" en lugar de "/poliservicios/" para rutas relativas
+  build: {
+    outDir: "dist",
+  },
+});
