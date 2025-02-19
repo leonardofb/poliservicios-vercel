215e012339785df7645df988ac2eee6e25056a46
leonardo fierro <planeaweb@gmail.com>
2025-02-19 03:12:14 +0000

    cambios en la rama original

--- mipaginaweb/src/main.jsx
+++ mipaginaweb/src/main.jsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
@@ -12,16 +11,3 @@ ReactDOM.render(
     </React.StrictMode>,
     document.getElementById('app')
 );
-/*
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css"; 
-
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-*/
