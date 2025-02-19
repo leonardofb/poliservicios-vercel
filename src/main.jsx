4615357e0f8e9db5698d3a566fd6094da0398e01
leonardo fierro <planeaweb@gmail.com>
2025-02-19 17:25:28 +0000

    Ya presta un mejor funcionamiento falta organizar home

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App';
 import './index.css'; // Si tienes estilos globales
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
-import './assets/css/style-starter.css'; // Importar estilos globales
+//import './assets/css/style-starter.css';// Importar estilos globales
 const root = ReactDOM.createRoot(document.getElementById("app"));
 
 root.render(
