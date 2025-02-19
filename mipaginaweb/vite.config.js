d75f3fbc20d96527a5bfd7b439737aab566d039f
leonardo fierro <planeaweb@gmail.com>
2025-02-19 04:24:24 +0000

    Corrigiendo configuración de despliegue

--- mipaginaweb/vite.config.js
+++ mipaginaweb/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/poliservicios/",
+  base: "/poliservicios/mipaginaweb/",
   plugins: [react()],
 })
