db1d5696cc5078f605ad991a759ad3509db1ebb9
LEONARDO FIERRO BERMUDEZ <127811621+leonardofb@users.noreply.github.com>
2025-02-23 07:30:43 +0000

    ya se migro el html de testimoniales ya falta poco

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,57 +1,52 @@
 import React from 'react';
+import styles from './Testimonials.module.css';
+
+const testimonials = [
+    {
+        name: "John Wilson",
+        city: "Example City",
+        quote: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit beatae laudantium voluptate rem ullam dolore nisi voluptatibus esse quasi, doloribus tempora.",
+        img: "assets/images/team1.jpg"
+    },
+    {
+        name: "Julia Sakura",
+        city: "Example City",
+        quote: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit beatae laudantium voluptate rem ullam dolore nisi voluptatibus esse quasi, doloribus tempora.",
+        img: "assets/images/team2.jpg"
+    }
+];
 
 const Testimonials = () => {
     return (
-        <section className="w3l-clients pb-5" id="clients">
-            <div className="cusrtomer-layout py-5 pb-0">
-                <div className="container py-lg-4 py-md-3 ">
-                    <div className="heading text-center mx-auto">
-                        <h6 className="title-subhny mb-2"><span>Testimonials</span></h6>
-                        <h3 className="title-w3l mb-5">Happy Clients
-                            <span className="span-bold">& Feedbacks</span>
-                        </h3>
-                    </div>
-                    <div className="testimonial-width">
-                        <div className="row">
-                            <div className="col-lg-6 item">
-                                <div className="testimonial-content">
-                                    <div className="testimonial">
+        <section className={styles.testimonialsSection}>
+            <div className={styles.testimonialsContainer}>
+                <div className="heading text-center mx-auto">
+                    <h6 className="title-subhny mb-2"><span>Testimonials</span></h6>
+                    <h3 className="title-w3l mb-5">Happy Clients <span className="span-bold">& Feedbacks</span></h3>
+                </div>
+                <div className={styles.testimonialWidth}>
+                    <div className="row">
+                        {testimonials.map((testimonial, index) => (
+                            <div className="col-lg-6 item" key={index}>
+                                <div className={styles.testimonialContent}>
+                                    <div className={styles.testimonial}>
                                         <blockquote>
-                                            <q><i className="fas fa-quote-left"></i> Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit beatae
-                                                laudantium
-                                                voluptate rem ullam dolore nisi voluptatibus esse quasi, doloribus tempora.
-                                                Dolores molestias adipisci dolor sit amet!.</q>
+                                            <i className="fas fa-quote-left"></i>
+                                            <q>{testimonial.quote}</q>
                                         </blockquote>
-                                        <div className="testi-des">
-                                            <div className="test-img"><img src="assets/images/team1.jpg" className="img-fluid" alt="client-img" /></div>
-                                            <div className="peopl align-self">
-                                                <h3>John Wilson</h3>
-                                                <p className="indentity">Example City</p>
+                                        <div className={styles.testiDes}>
+                                            <div className={styles.testImg}>
+                                                <img src={testimonial.img} className="img-fluid" alt="client-img" />
+                                            </div>
+                                            <div className={styles.peopl}>
+                                                <h3>{testimonial.name}</h3>
+                                                <p className={styles.identity}>{testimonial.city}</p>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            <div className="col-lg-6 item">
-                                <div className="testimonial-content">
-                                    <div className="testimonial">
-                                        <blockquote>
-                                            <q><i className="fas fa-quote-left"></i> Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit beatae
-                                                laudantium
-                                                voluptate rem ullam dolore nisi voluptatibus esse quasi, doloribus tempora.
-                                                Dolores molestias adipisci dolor                                                 dolor sit amet!.</q>
-                                        </blockquote>
-                                        <div className="testi-des">
-                                            <div className="test-img"><img src="assets/images/team2.jpg" className="img-fluid" alt="client-img" /></div>
-                                            <div className="peopl align-self">
-                                                <h3>Julia Sakura</h3>
-                                                <p className="indentity">Example City</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        ))}
                     </div>
                 </div>
             </div>
@@ -60,3 +55,5 @@ const Testimonials = () => {
 };
 
 export default Testimonials;
+
+
--- src/components/TestimonialsSection.jsx
+++ src/components/TestimonialsSection.jsx
@@ -1,53 +1,52 @@
-// src/components/TestimonialsSection.jsx
 import React from 'react';
+import styles from './Testimonials.module.css';
 
-const TestimonialsSection = () => {
-    const testimonials = [
-        {
-            name: "John Wilson",
-            city: "Example City",
-            quote: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit beatae laudantium voluptate rem ullam dolore nisi voluptatibus esse quasi, doloribus tempora.",
-            img: "assets/images/team1.jpg"
-        },
-        {
-            name: "Julia Sakura",
-            city: "Example City",
-            quote: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit beatae laudantium voluptate rem ullam dolore nisi voluptatibus esse quasi, doloribus tempora.",
-            img: "assets/images/team2.jpg"
-        }
-    ];
+const testimonials = [
+    {
+        name: "John Wilson",
+        city: "Example City",
+        quote: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit beatae laudantium voluptate rem ullam dolore nisi voluptatibus esse quasi, doloribus tempora.",
+        img: "assets/images/team1.jpg"
+    },
+    {
+        name: "Julia Sakura",
+        city: "Example City",
+        quote: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit beatae laudantium voluptate rem ullam dolore nisi voluptatibus esse quasi, doloribus tempora.",
+        img: "assets/images/team2.jpg"
+    }
+];
 
+const Testimonials = () => {
     return (
-        <section className="w3l-clients pb-5" id="clients">
-            <div className="cusrtomer-layout py-5 pb-0">
-                <div className="container py-lg-4 py-md-3">
-                    <div className="heading text-center mx-auto">
-                        <h6 className="title-subhny mb-2"><span>Testimonials</span></h6>
-                        <h3 className="title-w3l mb-5">Happy Clients <span className="span-bold">& Feedbacks</span></h3>
-                    </div>
-                    <div className="testimonial-width">
-                        <div className="row">
-                            {testimonials.map((testimonial, index) => (
-                                <div className="col-lg-6 item" key={index}>
-                                    <div className="testimonial-content">
-                                        <div className="testimonial">
-                                            <blockquote>
-                                                <q><i className="fas fa-quote-left"></i> {testimonial.quote}</q>
-                                            </blockquote>
-                                            <div className="testi-des">
-                                                <div className="test-img">
-                                                    <img src={testimonial.img} className="img-fluid" alt="client-img" />
-                                                </div>
-                                                <div className="peopl align-self">
-                                                    <h3>{testimonial.name}</h3>
-                                                    <p className="indentity">{testimonial.city}</p>
-                                                </div>
+        <section className={styles.testimonialsSection}>
+            <div className={styles.testimonialsContainer}>
+                <div className="heading text-center mx-auto">
+                    <h6 className="title-subhny mb-2"><span>Testimonials</span></h6>
+                    <h3 className="title-w3l mb-5">Happy Clients <span className="span-bold">& Feedbacks</span></h3>
+                </div>
+                <div className={styles.testimonialWidth}>
+                    <div className="row">
+                        {testimonials.map((testimonial, index) => (
+                            <div className="col-lg-6 item" key={index}>
+                                <div className={styles.testimonialContent}>
+                                    <div className={styles.testimonial}>
+                                        <blockquote>
+                                            <i className="fas fa-quote-left"></i>
+                                            <q>{testimonial.quote}</q>
+                                        </blockquote>
+                                        <div className={styles.testiDes}>
+                                            <div className={styles.testImg}>
+                                                <img src={testimonial.img} className="img-fluid" alt="client-img" />
+                                            </div>
+                                            <div className={styles.peopl}>
+                                                <h3>{testimonial.name}</h3>
+                                                <p className={styles.identity}>{testimonial.city}</p>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                            ))}
-                        </div>
+                            </div>
+                        ))}
                     </div>
                 </div>
             </div>
@@ -55,4 +54,6 @@ const TestimonialsSection = () => {
     );
 };
 
-export default TestimonialsSection;
+export default Testimonials;
+
+
