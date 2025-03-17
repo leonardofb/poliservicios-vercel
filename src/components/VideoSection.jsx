a76d0c23ec79fde327b51c1ffb6d4738f9c71e95
Leonardo Fierro <poliservicios@gmail.com>
2025-03-16 23:39:29 -0500

    Actualización del proyecto vercel

--- src/components/VideoSection.jsx
+++ src/components/VideoSection.jsx
@@ -10,7 +10,7 @@ const VideoSection = () => {
             <div className="position-relative py-5 my-lg-5">
               {/* Video centrado con controles */}
               <video controls className={styles.video}>
-                 <source src="../../src/assets/videos/mivideo.mp4" type="video/mp4" />
+                 <source src="/videos/mivideo.mp4" type="video/mp4" />
                 Tu navegador no soporta este formato de video.
               </video>
             </div>
--- vite.config.js
+++ vite.config.js
@@ -13,10 +13,54 @@ export default defineConfig({
   plugins: [react()],
   
   // Base correcta para GitHub Pages y local
+  // base: mode === "production" ? "/poliservicios/" : "/", // Solo para GitHub Pages
+
+  // Incluir archivos multimedia en la compilación
+  assetsInclude: ["**/*.mp4", "**/*.webm", "**/*.ogg"],
+
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    emptyOutDir: true, // Limpia "dist/" antes de construir
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
+      output: {
+        assetFileNames: "assets/[name]-[hash][extname]", // Nombre con hash para evitar caché
+      },
+    },
+  },
+  server: {
+    port: 3000,
+    strictPort: true,
+    open: true, // Abre automáticamente en el navegador
+  },
+});
+
+
+
+
+
+/*import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import { fileURLToPath } from "url";
+
+// Obtener la ruta absoluta del directorio actual
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
+
+// Verificar si import.meta.env está disponible
+const mode = import.meta.env ? import.meta.env.MODE : "development"; // Fallback a "development"
+
+export default defineConfig({
+  plugins: [react()],
+  
+  // Base correcta para GitHub Pages y local
  // base: mode === "production" ? "/poliservicios/" : "/", ESTA HAY QUE COLOCARLO SI CORREMOS EN GITPAGES
 
   // Mantener los archivos de public/
-  publicDir: "public",
+  publicDir: "src/assets",
 
   build: {
     outDir: "dist",
@@ -36,4 +80,4 @@ export default defineConfig({
     strictPort: true,
     open: true, // Abre automáticamente en el navegador
   },
-});
+});*/
